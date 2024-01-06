b405d307e9994b0380caf98f6b9c6cca4165da11
Karl Meissner <karl.meissner@indexexchange.com>
2024-01-06 15:01:46 -0500

    remove file and add comment

--- lessons/typescript/stacks/reverse_sentence.ts
+++ lessons/typescript/stacks/reverse_sentence.ts
@@ -1,3 +1,5 @@
+
+// For more details, see: [reverse_sentence.md](../../problems/stacks/reverse_sentence.md)
 function reverse(sentence: string): string {
   let fwd: string[] = sentence.split(' ');
   let rev : string[] = [];
